a7b830ac1445a1720ef188aa52373596dcb9ba83
Ved-chikhaliya <vkchikhaliya@gmail.com>
2025-02-19 11:05:48 -0800

    new file

---
+++ _includes/header.php
@@ -0,0 +1,1 @@
+<h1>Boss furniture</h1>
---
+++ index.php
@@ -0,0 +1,17 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+    </head>
+    <body>
+      <?php include("_includes/header.php")?>
+      <main>
+        <p>Boss atHome is designed to embody the new ways we live and work, making beautifully designed, sustainable furniture in the UK.
+Human centric furniture, designed for living.
+Established in 1983
+</p>
+      </main>
+    </body>
+    </html>
